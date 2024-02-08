2e6d87a57fd9b60dc4b6465c3d4ef8dc3f3d99d2
GabrielJaja <gabriel.jaja25@ycdsbk12.ca>
2024-02-08 12:47:54 -0500

    Updated PrintingComments.java

--- PrintingComments.java
+++ PrintingComments.java
@@ -8,13 +8,36 @@ class PrintingComments extends ConsoleProgram {
   public void run() {
 
     // Say hello
+    System.out.println("Hi ");
+    System.out.println("There");
     
 
     // Say hello on the same line
-    
+    System.out.print("Hi");
+    System.out.print("ere");
     
     // multiline comment
+    /*
+     * This is 
+     * a multiliine 
+     * comment
+     */
     
+// Draw a box
+System.out.println("*************");
+System.out.println("*           *");
+System.out.println("*           *");
+System.out.println("*           *");
+System.out.println("*************");
+      
+// Draw another box
+/*
+System.out.println("*************");
+System.out.println("*           *");
+System.out.println("*           *");
+System.out.println("*           *");
+System.out.println("*************");
+*/
     
 
   }
